7543e59663b6b5d180e0ff2c90eb99eb0af2b6a9
Khola Amjad <kzoe82826@gmail.com>
2023-11-05 10:31:20 +0500

    Added multiple users chatting functionality

--- lib/lets_chat_web/room_live/index.ex
+++ lib/lets_chat_web/room_live/index.ex
@@ -4,20 +4,39 @@ defmodule LetsChatWeb.RoomLive.Index do
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room: " <> room_id
-    LetsChatWeb.Endpoint.subscribe(topic)
-    {:ok, assign(socket, room_id: room_id, form: to_form(%{}, as: :message), topic: topic, messages: ["A user joined chat"])}
+
+    if connected?(socket) do
+      LetsChatWeb.Endpoint.subscribe(topic)
+    end
+
+    {:ok,
+     assign(socket,
+       room_id: room_id,
+       form: to_form(%{}, as: :message),
+       topic: topic,
+       messages: [],
+       username: username()
+     )}
   end
 
   @impl true
   def handle_event("submit_message", %{"message" => message}, socket) do
     message = Map.values(message)
-    updated_messages = socket.assigns.messages ++ [message]
-    LetsChatWeb.Endpoint.broadcast(socket.assigns.topic, "new_message", message)
-    {:noreply, assign(socket, messages: updated_messages)}
+
+    LetsChatWeb.Endpoint.broadcast(socket.assigns.topic, "new_message", %{
+      message: message,
+      name: socket.assigns.username
+    })
+
+    {:noreply, socket}
   end
 
   @impl true
-  def handle_info(%{event: "new_message", payload: _message}, socket) do
-    {:noreply, socket}
+  def handle_info(%{event: "new_message", payload: message}, socket) do
+    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
+  end
+
+  defp username() do
+    "#{MnemonicSlugs.generate_slug(2)}"
   end
 end
